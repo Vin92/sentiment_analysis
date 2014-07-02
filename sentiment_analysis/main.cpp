a895bd8a95f5fe8ed2b7715f6035c3f8296b9c34
Vin92 <aleksey.vinogradow2011@yandex.ru>
2014-07-03 00:01:05 +0400

    Hello word! Пошла жара2!

--- sentiment_analysis/main.cpp
+++ sentiment_analysis/main.cpp
@@ -17,6 +17,14 @@ int main(int argc, char** argv) {
 
     cout<<"Hello word!"<<endl<<"лох!";
     //странно...
+    
+    
+    cout<<endl<<endl;;
+    for (int i=0; i< 10; i++){
+        cout<<"LOX"<<endl;
+    }
+    
+    
     return 0;
 }
 
