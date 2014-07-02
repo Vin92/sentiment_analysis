c1d483c8b11c88c82c65b74609ea6a206df45007
Vin92 <aleksey.vinogradow2011@yandex.ru>
2014-07-02 22:29:58 +0400

    Hello word! Пошла жара!

---
+++ sentiment_analysis/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Vin92
+ *
+ * Created on 2 Июль 2014 г., 21:50
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout<<"Hello word!";
+    
+    return 0;
+}
+
