0f6153e291ebf66903ce22e1bd53bb65291fcc7d
Кирилл <@Home-pc>
2014-07-03 16:19:50 +0400

    начал подкулючать библиотеку

--- sentiment_analysis/vkParser.cpp
+++ sentiment_analysis/vkParser.cpp
@@ -1,8 +1,16 @@
 #include "vkParser.h"
 
 using namespace std;
+
+
 int Parser()
 {
+    HINSTANCE hLib=LoadLibrary("libvkparser.DLL");// кинь библиотеку к exe файлу
+    if(hLib==NULL) 
+    {
+       cout << "Unable to load library!" << endl;
+    }
+    // я хз чё дальше, задолбался, дальше от функции зависит
     
 }
 
